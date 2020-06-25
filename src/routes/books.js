b5701208c10441c2b9d53b4cc55a592beaec0bcb
Vikra Ardiansyah <vikraardi@gmail.com>
2020-06-26 00:08:32 +0700

    auth for get book

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const Route = express.Router()
+const express = require("express");
+const Route = express.Router();
 
-const booksController = require('../controllers/books');
-const upload =require('../helpers/fileUpload')
-const { authentication, authorization } = require('../middleware/auth')
+const booksController = require("../controllers/books");
+const upload = require("../helpers/fileUpload");
+const { authentication, authorization } = require("../middleware/auth");
 
+Route.get("/", booksController.getBooks)
+  .post("/", authentication, authorization, upload, booksController.postBook)
+  .put("/:id", authentication, authorization, upload, booksController.putBook)
+  .delete("/:id", authentication, authorization, booksController.deleteBook);
 
-
-Route
-    .get('/', authentication, booksController.getBooks)
-    .post('/', authentication, authorization, upload,booksController.postBook)
-    .put('/:id', authentication, authorization, upload,booksController.putBook)
-    .delete('/:id', authentication, authorization, booksController.deleteBook)
-
-
-
-    module.exports = Route
+module.exports = Route;
