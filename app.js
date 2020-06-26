27a186e61626ff4200daea11a9f538d6549cfe26
Vikra Ardiansyah <vikraardi@gmail.com>
2020-06-26 22:40:16 +0700

    express static

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan')
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 // const connection = mysql.createConnection({
 //     host: process.env.DB_HOST,
 //     user: process.env.DB_USER,
 //     password: process.env.DB_PASSWORD,
 //     database: process.env.DB_DATABASE
 // });
-const routeNavigator = require('./src/index')
-const server = app.listen(process.env.PORT, process.env.HOST, function() {
-    const host = server.address().address
-    const port = server.address().port
+const routeNavigator = require("./src/index");
+const server = app.listen(process.env.PORT, process.env.HOST, function () {
+  const host = server.address().address;
+  const port = server.address().port;
 
-    console.log("You're Connected at " + host + ":" + port)
-})
+  console.log("You're Connected at " + host + ":" + port);
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //  connection.connect(function(error) {
@@ -23,16 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     console.log('Database has Connected')
 
 // })
-
-app.use(morgan("dev"))
-app.use(cors({
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(
+  cors({
     origin: "*",
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    credentials: 'true',
-    optionSuccessStatus: 200
-
-}))
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: "true",
+    optionSuccessStatus: 200,
+  })
+);
 // app.get('/',function (requset,response) {
 //     connection.query('SELECT * FROM books', function(error,result){
 //         if(error){
@@ -40,8 +41,7 @@ app.use(cors({
 //         }
 //         response.json(result);
 //     })
-    
-// })
-app.use('/',routeNavigator)
-module.exports = app
 
+// })
+app.use("/", routeNavigator);
+module.exports = app;
