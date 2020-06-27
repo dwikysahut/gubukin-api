d2a74ddd89e51074528a8b1c900a946a78b5f187
dwikysahut <dwikysahut@gmail.com>
2020-06-27 12:43:18 +0700

    rating system

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const bookRoutes = require("./routes/books");
 const authorRoutes = require("./routes/author");
 const categoryRoutes = require("./routes/category");
 const authRoutes = require('./routes/auth')
+const ratingRoutes = require('./routes/rating')
+
 const transactionRoutes = require('./routes/transactions')
 
 Route.use("/books", bookRoutes);
@@ -13,5 +15,7 @@ Route.use("/author", authorRoutes);
 Route.use("/category", categoryRoutes);
 Route.use('/user',userRoutes);
 Route.use('/auth', authRoutes)
+Route.use('/rating', ratingRoutes)
+
 Route.use('/transaction',transactionRoutes)
 module.exports = Route
