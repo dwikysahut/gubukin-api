07c0e6b4672d5c86a64c181f09bd00e8dda1ca59
Vikra Ardiansyah <vikraardi@gmail.com>
2020-06-27 16:24:58 +0700

    get user by id

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,7 @@ const upload = require("../helpers/fileUpload");
 const { authentication } = require("../middleware/auth");
 
 Route.get("/", authentication, userController.getUser)
+  .get("/:id", authentication, userController.getUserById)
   .post("/", authentication, upload, userController.postUser)
   .put("/:id", authentication, upload, userController.putUser)
   .delete("/:id", authentication, userController.deleteUser);
